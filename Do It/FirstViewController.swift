28c164a6b19eef31279a1d11df9b233f8161ce39
Snigdhaman Chatterjee <snigdhaman1991@gmail.com>
2015-12-26 14:53:11 +0530

    Initial Commit

---
+++ Do It/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Do It
+//
+//  Created by Chatterjee, Snigdhaman on 26/12/15.
+//  Copyright © 2015 Chatterjee, Snigdhaman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Do It/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Do It
+//
+//  Created by Chatterjee, Snigdhaman on 26/12/15.
+//  Copyright © 2015 Chatterjee, Snigdhaman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
