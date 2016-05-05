923d2954ac2f5de5e6e3f6973a717090bcffa1dd
Snigdhaman Chatterjee <snigdhaman1991@gmail.com>
2016-05-05 20:03:46 +0530

    Simple to do app called "Do It!!"

--- Do It/FirstViewController.swift
+++ Do It/FirstViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var toDoList = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet weak var doItTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil {
+            toDoList = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return toDoList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "\(indexPath.row + 1).  \(toDoList[indexPath.row])"
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            toDoList.removeAtIndex(indexPath.row)
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            doItTable.reloadData()
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +50,11 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        
+        doItTable.reloadData()
+        
+    }
 
 }
 
--- Do It/SecondViewController.swift
+++ Do It/SecondViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var itemTextField: UITextField!
+    
+    
+    @IBAction func addItemButton(sender: AnyObject) {
+        
+        if !(itemTextField.text!.isEmpty) {
+            toDoList.append(itemTextField.text!)
+            itemTextField.text = ""
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.itemTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,18 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+
+    }
 
 }
 
